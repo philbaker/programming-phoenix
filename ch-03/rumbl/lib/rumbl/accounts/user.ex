6acff24c23e0dd45fb59e78b203d3a20f424ca03
philbaker <phil@philbaker.me>
2022-05-11 07:30:01 +0100

    Add authentication

--- ch-03/rumbl/lib/rumbl/accounts/user.ex
+++ ch-03/rumbl/lib/rumbl/accounts/user.ex
@@ -5,6 +5,8 @@ defmodule Rumbl.Accounts.User do
   schema "users" do
     field :name, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps()
   end
@@ -14,6 +16,24 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  def put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+      _ ->
+        changeset
+    end
   end
 end
 
@@ -87,3 +107,70 @@ end
 #   username: "josevalim"
 # }
 
+# Changesets
+# alias Rumbl.Accounts.User
+
+# alias RumblWeb.Router.Helpers, as: Routes
+
+# changeset = User.registration_changeset(%User{}, %{
+#   username: "max", name: "Max", password: "123"
+# })
+##Ecto.Changeset<
+#  action: nil,
+#  changes: %{
+#    name: "Max",
+#    password: "123",
+#    username: "max"
+#  },
+#  errors: [
+#    password: {"should be at least %{count} charact
+#er(s)",
+#     [
+#       count: 6,
+#       validation: :length,
+#       kind: :min,
+#       type: :string
+#     ]}
+#  ],
+#  data: #Rumbl.Accounts.User<>,
+#  valid?: false
+#>
+
+# changeset = User.registration_changeset(%User{}, %{
+#   username: "max", name: "Max", password: "asecret"
+# })
+##Ecto.Changeset<
+#  action: nil,
+#  changes: %{
+#    name: "Max",
+#    password: "asecret",
+#    password_hash: "$pbkdf2-sha512$160000$ADwgbHL8m
+#cpITuKPDidIjw$BI.vgsxYF4n8ltxR5JVcuSyDiphlxY3cQXv/.
+#0baDZ16sglW6yQwLEtww6mYJRmR5eUbyW0UsWMKpNIdjXm/aw",
+#    username: "max"
+#  },
+#  errors: [],
+#  data: #Rumbl.Accounts.User<>,
+#  valid?: true
+#>
+
+# changeset.valid?
+# true
+
+# changeset.changes
+# %{
+#   name: "Max",
+#   password: "asecret",
+#   password_hash: "$pbkdf2-sha512$160000$ADwgbHL8mcp
+# ITuKPDidIjw$BI.vgsxYF4n8ltxR5JVcuSyDiphlxY3cQXv/.0b
+# aDZ16sglW6yQwLEtww6mYJRmR5eUbyW0UsWMKpNIdjXm/aw",
+#   username: "max"
+# }
+
+# Ensure existing users have a password
+
+# alias Rumbl.Repo
+
+# for u <- Repo.all(User) do
+#   Repo.update!(User.registration_changeset(u, %{password: "temppass"}))
+# end
--- ch-03/rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ ch-03/rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -2,6 +2,8 @@ defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
   alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
+  plug :authenticate when action in [:index, :show]
 
   def index(conn, _params) do
     users = Accounts.list_users()
@@ -13,22 +15,32 @@ defmodule RumblWeb.UserController do
     render(conn, "show.html", user: user)
   end
 
-  alias Rumbl.Accounts.User
-
   def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
+    changeset = Accounts.change_registration(%User{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
+    case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
   end
 end
