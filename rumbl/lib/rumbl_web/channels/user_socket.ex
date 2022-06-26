bcbc562f61ab4ab598ddd9fbb963ff767c5c88e0
philbaker <phil@philbaker.me>
2022-06-26 13:42:05 +0100

    Connect via websockets

---
+++ rumbl/lib/rumbl_web/channels/user_socket.ex
@@ -0,0 +1,10 @@
+defmodule RumblWeb.UserSocket do
+  use Phoenix.Socket
+
+  # channel "room:*", RumblWeb.RoomChannel
+
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+  def id(_socket), do: nil
+end
